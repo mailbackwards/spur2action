28f4b94b561a82d33a5c762c75ef96df115f140e
Liam Andrew <landrew@texastribune.org>
2016-08-27 16:34:32 -0500

    Add pre-k interactions for other user story

--- data.py
+++ data.py
@@ -1,8 +1,41 @@
 MESSAGES = {
-    'prop1': 'Prop 1 is on Saturday, May 7. Practice voting and see what happens! Text YES or NO.'
+    'prop1': 'Prop 1 is on Saturday, May 7. Practice voting and see what happens! Text YES or NO.',
+    'prop1-snack': "Reducing the number of cars on SA's roads will help move people faster.",
+    'prop1-prevote': ('The ridesharing election is tomorrow! '
+        "Send us your zip code and we'll let you know where your closest polling place is."),
+    'prek': 'Prop PREK4SA is on Tuesday, November 6th. Practice voting and see what happens! Text Y or N.',
+    'prek-prevote': ('The PreK4SA election is tomorrow! '
+        "Send us your zip code and we'll let you know where your closest polling place is.")
 }
 
 REPLIES = {
+    "E": {
+        "body": (
+            "So glad to hear you're thinking about your student's future now! So are we! "
+            "We're S2A, Spur to Action, aiming to inform you about the issues that are important to you. "
+            "Look forward to hearing more about the latest education issue coming up in your area. "
+            "Learn more about us at http://texasturnout.herokuapp.com. "
+            "Reply STOP at any time, and we won't send you any more updates!"
+        ),
+        "media": []
+    },
+    "Y": {
+        "body": (
+            'Voting "yes" will increase funding so an additional 5,700 students can attend full-day Pre-K programs. '
+            'The funding for this will be created by a small sales tax increase.\n\n'
+            'Try "N" to see what the other side argues, or "MORE" for other info.'
+        ),
+        "media": []
+    },
+    "N": {
+        "body": (
+            'Voting "no" will maintain sales tax rates as they are (8.125%) '
+            'and maintain the current figures of students attending Pre-K.\n\n'
+            'Try "Y" to see what the other side argues, or "MORE" for other info.'
+        ),
+        "media": []
+    },
+
     "T": {
         "body": (
             "So glad to hear you hate traffic! We do too!  We're S2A, Spur to Action, "
@@ -32,7 +65,7 @@ REPLIES = {
         "media": []
     },
     "MORE": {
-        "body": 'Here is more info about transportation in San Antonio!',
+        "body": 'Here is more info about local issues in San Antonio!',
         "media": []
     },
     "STOP": {
