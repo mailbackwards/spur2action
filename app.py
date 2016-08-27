fa5da1b11d099bf171c48870e4fd55d9c935a8c1
Liam Andrew <landrew@texastribune.org>
2016-08-27 13:22:49 -0500

    Test twiml endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, render_template
 from twilio.rest import TwilioRestClient
+from twilio import twiml
 
 TWILIO_PHONE_NUMBER='+12036800787'
 
@@ -20,6 +21,14 @@ def send_text_message():
     body = request.form.get('body')
     send_text('+12036067072', body)
     return 'Success!'
+
+@app.route('/listen', methods=['GET'])
+def receive_text():
+    #content = request.form.get('content')
+
+    resp = twiml.Response()
+    resp.say('hello', sender=TWILIO_PHONE_NUMBER)
+    return str(resp)
 
 if __name__ == '__main__':
     app.run(debug=bool(os.environ.get('DEBUG')))
